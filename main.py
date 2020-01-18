6bea17a891a482b62506e32dab9963323a2951f1
Matus Stas <matusstas.ms@gmail.com>
2020-01-18 19:57:53 +0100

    Refactoring

--- main.py
+++ main.py
@@ -1,72 +1,13 @@
-from PIL import Image, ImageDraw
 from tkinter import *
 import random
-import numpy
 import math
 import time
 
-# size = 10
-# arr = []
-
-# img = Image.new("RGB", (800,800), "black")
-# draw = ImageDraw.Draw(img)
-
-# for i in range(100):
-# 	x = random.randrange(800-size)
-# 	y = random.randrange(800-size)
-# 	arr.append([x,y])
-
-# for x,y in arr:
-# 	draw.ellipse((x, y, x+size, y+size), fill = 'white')
-
-# arr.sort()
-# x,y = arr[-1]
-# # draw.ellipse((x, y, x+size, y+size), fill = 'green')
-
-# S = arr[0]
-# E = arr[-1]
-# start = arr[0]
-# MAX = []
-# ok = 0
-# while True:
-# 	maxd = 0
-# 	j = []
-# 	for i in range(0,len(arr)):
-# 		if arr[i] == start:
-# 			continue
-# 		X = arr[i][0]-start[0]
-# 		Y = arr[i][1]-start[1]
-# 		degree = math.degrees(math.atan2(X,Y))
-# 		# print(start,arr[i],degree)
-# 		MAX.append([degree,arr[i]])
-# 		if degree > maxd:
-# 			maxd = degree
-# 			j = arr[i]
-# 	MAX.sort()
-# 	if MAX[0][0] < 0 and MAX[-1][0] > 0 and ok == 1:
-# 		q = -1
-# 		while MAX[q][0] > 0:
-# 			q -= 1
-# 		j = MAX[q][1]
-
-# 	else:
-# 		j = MAX[-1][1]
-# 	MAX = []
-# 	draw.line((start[0]+size//2,start[1]+size//2,j[0]+size//2,j[1]+size//2))
-# 	if start != S:
-# 		arr.pop(arr.index(start))
-# 	start = j
-# 	if start == E:
-# 		ok = 1
-# 	if start == S:
-# 		break
-
-# img.show()
 
 width = 500
 height = 500
 size = 10
-n = 50
+n = 20
 arr = []
 
 root = Tk()
@@ -107,7 +48,7 @@ while True:
 
 		if arr[i] != S:
 			screen.create_line((start[0]+size//2,start[1]+size//2,arr[i][0]+size//2,arr[i][1]+size//2), fill = "white")
-			time.sleep(0.001)
+			time.sleep(0.01)
 
 			for l in XX:
 				screen.create_oval(l[0],l[1],l[0]+size,l[1]+size, width = 0, fill = "white")
@@ -115,7 +56,7 @@ while True:
 			
 
 			screen.create_line((start[0]+size//2,start[1]+size//2,arr[i][0]+size//2,arr[i][1]+size//2), fill = "black")
-			time.sleep(0.001)
+			time.sleep(0.01)
 			for l in XX:
 				screen.create_oval(l[0],l[1],l[0]+size,l[1]+size, width = 0, fill = "white")
 			
