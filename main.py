2f2a25af384498d3664ba6158d9b07ca7b1b7042
Matus Stas <matusstas.ms@gmail.com>
2020-01-19 13:46:43 +0100

    Refactoring

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ import time
 
 size = 500
 radius = 10
+center = radius//2
 n = 32
 arr = []
 
+def extractCoorinates(actual_point, next_point, center):
+	actual_point_coordinates = [x+center for x in actual_point]
+	next_point_coordinates = [x+center for x in next_point]
+	return actual_point_coordinates, next_point_coordinates
 
 def LeftRight():
 	arr.sort()
@@ -32,11 +37,11 @@ screen.pack()
 
 pointGenerator()
 
-left,right = LeftRight()
+left_point,right_point = LeftRight()
 
 XX = arr[:]
-edge = []
-start = left
+edges = []
+actual_point = left_point
 MAX = []
 ok = 0
 screen.update()
@@ -45,29 +50,29 @@ while True:
 	maxd = 0
 	next_point = []
 	for i in range(0,len(arr)):
-		if arr[i] == start:
+		if arr[i] == actual_point:
 			continue
 		
-		X = arr[i][0]-start[0]
-		Y = arr[i][1]-start[1]
+		X = arr[i][0]-actual_point[0]
+		Y = arr[i][1]-actual_point[1]
 		degree = math.degrees(math.atan2(X,Y))
 
-		if arr[i] != left:
-			screen.create_line((start[0]+radius//2,start[1]+radius//2,arr[i][0]+radius//2,arr[i][1]+radius//2), fill = "white")
+		if arr[i] != left_point:
+			screen.create_line((actual_point[0]+center,actual_point[1]+center,arr[i][0]+center,arr[i][1]+center), fill = "white")
 			time.sleep(0.01)
 
 			for l in XX:
 				screen.create_oval(l[0],l[1],l[0]+radius,l[1]+radius, width = 0, fill = "white")
 			screen.update()
 		
-			screen.create_line((start[0]+radius//2,start[1]+radius//2,arr[i][0]+radius//2,arr[i][1]+radius//2), fill = "black")
+			screen.create_line((actual_point[0]+center,actual_point[1]+center,arr[i][0]+center,arr[i][1]+center), fill = "black")
 			time.sleep(0.01)
 			
 			for l in XX:
 				screen.create_oval(l[0],l[1],l[0]+radius,l[1]+radius, width = 0, fill = "white")
 			
-			for e in edge:
-				screen.create_line(e, width = 2, fill = "white")
+			for edge in edges:
+				screen.create_line(edge, width = 2, fill = "white")
 
 			screen.update()
 
@@ -85,17 +90,18 @@ while True:
 	else:
 		next_point = MAX[-1][1]
 
-
 	MAX = []
-	screen.create_line((start[0]+radius//2,start[1]+radius//2,next_point[0]+radius//2,next_point[1]+radius//2), width = 2, fill = "white")
-	edge.append((start[0]+radius//2,start[1]+radius//2,next_point[0]+radius//2,next_point[1]+radius//2))
+
+	screen.create_line((actual_point[0]+center,actual_point[1]+center,next_point[0]+center,next_point[1]+center), width = 2, fill = "white")
+
+	edges.append(extractCoorinates(actual_point,next_point,center))
 	screen.update()
 	
-	start = next_point
+	actual_point = next_point
 	
-	if start == right:
+	if actual_point == right_point:
 		ok = 1
-	if start == left:
+	if actual_point == left_point:
 		break
 
 root.mainloop()
